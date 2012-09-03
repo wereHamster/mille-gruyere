5895f22834ee889f2be7dfffacb868eb872bb4f1
Tomas Carnecky <tomas.carnecky@gmail.com>
2012-09-03 21:29:29 +0200

    yawn

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module MilleGruyere
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.secret_token = "0" * 30
   end
 end
