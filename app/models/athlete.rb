6c0c7ab660791e9a6caa1fbf6c970a6fec6cc3bb
Tomas Carnecky <tomas.carnecky@gmail.com>
2012-09-06 10:06:28 +0200

    something..

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -13,10 +13,37 @@ class Athlete < ActiveRecord::Base
     end
   end
 
+  def pad3(value)
+    if value < 10
+      "00#{value}"
+    elsif value < 100
+      "0#{value}"
+    else
+      value.to_s
+    end
+  end
+
+  def pad2(value)
+    if value < 10
+      "0#{value}"
+    else
+      "#{value}"
+    end
+  end
+
   def zeit
     zeit = read_attribute(:zeit)
     if zeit
-      "#{zeit / 1000 / 100}:#{(zeit / 1000) % 100}.#{zeit % 1000}"
+      "#{zeit / 1000 / 100}:#{pad2((zeit / 1000) % 100)}.#{pad3(zeit % 1000)}"
+    else
+      nil
+    end
+  end
+
+  def zeit_slv
+    zeit = read_attribute(:zeit)
+    if zeit
+      "#{zeit / 1000 / 100}.#{pad2((zeit / 1000) % 100)}#{pad2((zeit % 1000) / 100)}"
     else
       nil
     end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,31 +1,33 @@
 # encoding: UTF-8
 
 class Category
-  attr_reader :name, :titel, :jahrgang, :geschlecht
+  attr_reader :name, :titel, :jahrgang, :geschlecht, :cat
 
 
   def self.all
     [
-      Category.new("K2003", "Knaben, 2003 und jünger",  (2003..2012), "Knabe"),
-      Category.new("M2003", "Mädchen, 2003 und jünger", (2003..2012), "Mädchen"),
+      Category.new("K2003", "Knaben, 2003 und jünger",  (2003..2012), "Knabe",   "M09"),
+      Category.new("M2003", "Mädchen, 2003 und jünger", (2003..2012), "Mädchen", "W09"),
 
-      Category.new("K2002", "Knaben, 2002",  (2002..2002), "Knabe"),
-      Category.new("M2002", "Mädchen, 2002", (2002..2002), "Mädchen"),
+      Category.new("K2002", "Knaben, 2002",  (2002..2002), "Knabe",   "M10"),
+      Category.new("M2002", "Mädchen, 2002", (2002..2002), "Mädchen", "W10"),
 
-      Category.new("K2001", "Knaben, 2001",  (2001..2001), "Knabe"),
-      Category.new("M2001", "Mädchen, 2001", (2001..2001), "Mädchen"),
+      Category.new("K2001", "Knaben, 2001",  (2001..2001), "Knabe",   "M11"),
+      Category.new("M2001", "Mädchen, 2001", (2001..2001), "Mädchen", "W11"),
 
-      Category.new("K2000", "Knaben, 2000",  (2000..2000), "Knabe"),
-      Category.new("M2000", "Mädchen, 2000", (2000..2000), "Mädchen"),
+      Category.new("K2000", "Knaben, 2000",  (2000..2000), "Knabe",   "M12"),
+      Category.new("M2000", "Mädchen, 2000", (2000..2000), "Mädchen", "W12"),
 
-      Category.new("K1999", "Knaben, 1999",  (1999..1999), "Knabe"),
-      Category.new("M1999", "Mädchen, 1999", (1999..1999), "Mädchen"),
+      Category.new("K1999", "Knaben, 1999",  (1999..1999), "Knabe",   "M13"),
+      Category.new("M1999", "Mädchen, 1999", (1999..1999), "Mädchen", "W13"),
 
-      Category.new("K1998", "Knaben, 1998",  (1998..1998), "Knabe"),
-      Category.new("M1998", "Mädchen, 1998", (1998..1998), "Mädchen"),
+      Category.new("K1998", "Knaben, 1998",  (1998..1998), "Knabe",   "M14"),
+      Category.new("M1998", "Mädchen, 1998", (1998..1998), "Mädchen", "W14"),
 
-      Category.new("K1997", "Knaben, 1997",  (1997..1997), "Knabe"),
-      Category.new("M1997", "Mädchen, 1997", (1997..1997), "Mädchen"),
+      Category.new("K1997", "Knaben, 1997",  (1997..1997), "Knabe",   "M15"),
+      Category.new("M1997", "Mädchen, 1997", (1997..1997), "Mädchen", "W15"),
+
+      Category.new("M1989", "Mädchen, 1989", (1989..1989), "Mädchen", ""),
     ]
   end
 
@@ -34,11 +36,12 @@ class Category
   end
 
 
-  def initialize(name, titel, jahrgang, geschlecht)
+  def initialize(name, titel, jahrgang, geschlecht, cat)
     @name       = name
     @titel      = titel
     @jahrgang   = jahrgang
     @geschlecht = geschlecht
+    @cat        = cat
   end
 
   def athletes
